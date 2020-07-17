a44a736a95734335b48d6f92f4c109609abb476d
ahsanshah <shahahsan56@gmail.com>
2020-07-17 20:35:26 +0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,7 +12,7 @@
 
 </head>
 <body>
-<div class='container'>
+
 <?php
 // $servername ="localhost:8889";
 // $username = "root";
@@ -23,7 +23,7 @@ include 'connect.php';
 // $conn = new mysqli($servername, $username, $password, $dbname);
  //Check connection
 include 'header.php';
-include 'state.php';
+echo "<div class='container'>";
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
@@ -41,9 +41,10 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 
+echo "</div>";
+
 include 'footer.php';
 ?>
-</div>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.4/cjs/popper.min.js" integrity="sha512-BO7V2K9oqbTnqEK9j/RiBzft8mcyj5XsfDgCmc0yymJQcBl4qhuR+TVgPL2pilyEqcMJxc8t0tp/lXGu9I0loA==" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
